1fbf1cf156b2d9287e8753749def1052c2c66b59
natasha3 <nata.sokol.84@list.ru>
2020-12-01 12:33:48 +0300

    factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,18 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+ 
+  for (let n = 0; n < 6; n++) {
+    
+    if (n / 2 != 0 && n % 2 == 0) continue;
+    alert(n);
+  }
+
+  let result = n;
+  for(let i = n - 1; i >= 1; --i) {
+    result *= i;
+  }
+
+  return result;
+
 }
